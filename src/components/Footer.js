e490e9ee78edbddade27a7a28b5265eff7429fac
nader rabhi <naderrabhi93@outlook.com>
2023-01-04 21:53:39 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+
+                    <p>
+                        Built by | &nbsp;
+
+                            Nader Rabhi
+                    
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
